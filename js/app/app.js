76d28c82ec0e4fe3b756d1d16267dfdf35fd853a
vander <vander.01010@hotmail.com>
2020-09-29 16:25:51 -0300

    start: Angularjs project

---
+++ js/app/app.js
@@ -0,0 +1,13 @@
+angular.module("test", [/*'ngRoute'*/])
+/* .config(function($routeProvider){
+    $routeProvider
+    .when("/home",{
+        templateUrl:"templates/home.html",
+        controller:"control"
+    })
+    .when("/contato", {
+        templateUrl:"templates/contato.html"
+    })
+
+    $routeProvider.otherwise({redirectTo:"/home"})
+})*/
